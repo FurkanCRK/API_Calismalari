4fa59d9be2480e4944d24a197fa585865bc53724
FurkanCRK <fcyazilim35@gmail.com>
2023-06-22 14:33:25 +0300

    22.06.2023

---
+++ src/test/java/test/C02_Get_ResponseBilgileriAssertion.java
@@ -0,0 +1,45 @@
+package test;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C02_Get_ResponseBilgileriAssertion {
+
+
+    @Test
+    public void get2 (){
+
+        /*
+        https://restful-booker.herokuapp.com/booking/10 url'ine bir GET request
+        gonderdiginizde donen Response'un,
+            status code'unun 200,
+            ve content type'inin application/json;charset=utf-8,
+            ve Server isimli Header'in degerinin Cowboy,
+            ve status Line'in HTTP/1.1 200 OK
+
+            oldugunu test edin.
+     */
+
+        // 1- Url Hazirla
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Expected Data hazirla
+
+        // 3- Response kaydet
+
+        Response response = given().when().get(url);
+
+        // response.prettyPrint();
+
+        // 4- Assertion
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json;charset=utf-8")
+                .header("Server","Cowboy")
+                .statusLine("HTTP/1.1 200 OK");
+
+
+    }
+}
